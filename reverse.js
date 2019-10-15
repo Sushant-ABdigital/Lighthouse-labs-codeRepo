ee6e5446807d9efd7de91036ca6d9d3f6d28e9fd
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-15 20:50:58 +0000

    Fixed reverse.js

--- reverse.js
+++ reverse.js
@@ -1,8 +1,12 @@
-var input = process.argv[2];
+let input = process.argv[2];
 
-if (input)
+const reverse = input => {
+  return input
+    .split("")
+    .reverse()
+    .join("");
+};
+
+if (input) {
   console.log(reverse(input));
-
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
 }
